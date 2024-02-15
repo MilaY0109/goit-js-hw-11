8d2dfc1634ca0c544cc55fda72511a11d526683f
MilaY0109 <yanovskyl@ukr.net>
2024-02-15 23:21:59 +0200

    mistakes

---
+++ src/js/render-function.js
@@ -0,0 +1,50 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+export function renderGallery(images) {
+  if (images.hits.length === 0) {
+    iziToast.show({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      messageColor: '#FAFAFB',
+      backgroundColor: '#EF4040',
+      position: 'topRight',
+    });
+  } else {
+    const item = images.hits
+      .map(
+        image => `<li class="gallery-item">
+          <a class="gallery-link" href="${image.largeImageURL}" >
+            <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" width="360" />
+          </a>
+          <div class="img-text">
+            <div class="img-info">
+              <h3>Likes</h3>
+              <p>${image.likes}</p>
+            </div>
+            <div class="img-info">
+              <h3>Views</h3>
+              <p>${image.views}</p>
+            </div>
+            <div class="img-info">
+              <h3>Comments</h3>
+              <p>${image.comments}</p>
+            </div>
+            <div class="img-info">
+              <h3>Downloads</h3>
+              <p>${image.downloads}</p>
+            </div>
+          </div>
+        </li>`
+      )
+      .join('');
+
+    gallery.innerHTML = item;
+    const lightBox = new SimpleLightbox('.gallery-link');
+    lightBox.refresh();
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-import './js/pixabay-api.js';
+import { fetchImage } from './js/pixabay-api.js';
+import { renderGallery } from './js/render-function.js';
+
+const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  loader.style.display = 'inline-block';
+  gallery.innerHTML = '';
+  const inputText = form.elements.search.value.trim();
+  fetchImage(inputText)
+    .then(images => {
+      renderGallery(images);
+      loader.style.display = 'none';
+    })
+    .catch(error => {
+      iziToast.error({ message: error.message });
+      loader.style.display = 'none';
+    });
+  form.reset();
+}
